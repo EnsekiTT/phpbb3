c7ee1a77c2ff6ee3498ad01c0bf38e446e9bc447
EnsekiTT <ensekitt@gmail.com>
2012-08-23 11:18:27 +0900

    画象のパスを変更するスクリプトと仮変更した画象

--- phpBB/images/filename.sh
+++ phpBB/images/filename.sh
@@ -7,6 +7,7 @@ do
     a=($line)
     PARA1=`echo ${a[0]}`
     PARA2=`echo ${a[1]} | sed -e "s/.*\.\([^.]*\)\$/\1/g"`
+    #サーバかドメインか何かしらのハッシュ値　全てのファイル共通っぽい
     PARA3="a732c32113e71006fd761db98b26f8a3_"
-    mv ${a[1]} ${PARA3}${PARA1}.${PARA2}
+    echo ${a[1]} ${PARA3}${PARA1}.${PARA2}
 done < $1
