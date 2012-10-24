ff22e844c54d43d3518c71a86f957ad0b32aae89
EnsekiTT <ensekitt@gmail.com>
2012-10-24 16:48:16 +0900

    新サーバ用ハッシュ付きイメージ

--- phpBB/images/avatars/filename.sh
+++ phpBB/images/avatars/filename.sh
@@ -8,6 +8,6 @@ do
     PARA1=`echo ${a[0]}`
     PARA2=`echo ${a[1]} | sed -e "s/.*\.\([^.]*\)\$/\1/g"`
     #サーバかドメインか何かしらのハッシュ値　全てのファイル共通っぽい
-    PARA3="d8912e605e2e7db7f105c555d546898e_"
+    PARA3="3c78571d28212a6ade8f1c77e0f12fbc_"
     mv ${a[1]} ${PARA3}${PARA1}.${PARA2}
 done < $1
